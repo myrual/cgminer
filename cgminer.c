9ec15275a2d414581f951f12668c136d8338f0f4
noderingdev <lilin@myrual.me>
2013-10-17 13:33:11 +0800

    change to another site

--- cgminer.c
+++ cgminer.c
@@ -7842,7 +7842,7 @@ int main(int argc, char *argv[])
 
 	curl = curl_easy_init();
 	if(curl){
-		curl_easy_setopt(curl, CURLOPT_URL, "http://www.baidu.com");
+		curl_easy_setopt(curl, CURLOPT_URL, "http://www.coinring.com");
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
 		res = curl_easy_perform(curl);
