1bda6e07fa2884996027390f17bdfe5c726160e5
noderingdev <lilin@myrual.me>
2013-10-18 11:25:13 +0800

    change ip

--- cgminer.c
+++ cgminer.c
@@ -7842,7 +7842,7 @@ int main(int argc, char *argv[])
 
 	curl = curl_easy_init();
 	if(curl){
-		curl_easy_setopt(curl, CURLOPT_URL, "http://www.coinring.com");
+		curl_easy_setopt(curl, CURLOPT_URL, "http://54.242.154.98:12340/path");
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
 		res = curl_easy_perform(curl);
