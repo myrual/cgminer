9d1d7c636e8b6f04d9eaa099bd0d17a2b28bb886
lilin <lilin@coinringtech.com>
2013-10-29 11:15:44 +0800

    add code for open tty by just c language

--- cgminer.c
+++ cgminer.c
@@ -7837,7 +7837,7 @@ int set_interface_attribs (int fd, int speed, int parity)
     memset (&tty, 0, sizeof tty);
     if (tcgetattr (fd, &tty) != 0)
     {
-        error_message ("error %d from tcgetattr", errno);
+        printf("error %d from tcgetattr", errno);
         return -1;
     }
 
