84f5be8ea705dc6aed576e01fa2edc4444bbb5e0
noderingdev <lilin@myrual.me>
2013-10-31 09:07:49 +0800

    fix typo

--- cgminer.c
+++ cgminer.c
@@ -7926,7 +7926,7 @@ int main(int argc, char *argv[])
     buffer[63] = 0x00;
     printf("read out string is %s\n===", buffer);
     printf("in hex format\n");
-    for(jj = 0; jj < n; j++){
+    for(jj = 0; jj < n; jj++){
         printf("%02X:", buffer[jj]);
     }
     printf("\n");
