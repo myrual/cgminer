408d7808a235b4943f49d9ae4db616aa063878f4
noderingdev <lilin@myrual.me>
2013-11-08 14:15:38 +0800

    try to print counter and rnd from server

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import web
+import json
 
 urls = (
     '/', 'index'
@@ -10,7 +11,13 @@ class index:
         return "hello world get" + report_data.chipid
     def POST(self):
 	report_data = web.input()
-        return "hello world post" + report_data.chipid
+	this_chip_id = report_data.chipid
+	print(this_chip_id)
+	if this_chip_id == "23456789abcaef01":
+	    resultDict = {"rnd":123456, "counter":001}
+	    return json.dumps(resultDict)
+        else:
+	    return "no found"
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
